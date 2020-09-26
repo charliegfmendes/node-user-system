a30c1cb3b5056608c2e88b655b06d71f7b0a034a
Gabriel Mendes <gabrielrfmendes@gmail.com>
2020-09-26 13:24:26 -0300

    deleteAccount.js created and working successfully

---
+++ src/deleteAccount.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+const { User } = require('../../../models');
+const Device = require('../../../src/models/Device');
+
+module.exports = async (req, res) => {
+    const id = req.accountId;
+    const account = await User.findOne({ where: { id } });
+    if (!req.body.password) {
+        res.json('Password is required')
+    } else if (!await bcrypt.compare(req.body.password, account.password)) {
+        res.json('Invalid password')
+    } else {
+        await User.destroy({where: { id }});
+        await Device.deleteMany({ userId: id });
+        res.json('Deleted account')
+    }
+}
