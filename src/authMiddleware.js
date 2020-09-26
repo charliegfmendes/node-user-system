3458a50638d534b0ce47d8324d15315e49c46d94
Gabriel Mendes <gabrielrfmendes@gmail.com>
2020-09-26 02:09:48 -0300

    authMiddleware.js created and working

---
+++ src/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+const Device = require('../../../src/models/Device');
+const { secret } = require('../../../src/secret.json');
+
+module.exports = async (req, res, next) => {
+    const { authorization } = req.headers;
+    const UA = req.get('User-Agent');
+    if (!authorization) {
+        res.json('Authentication is required')
+    } else {
+        let parts = authorization.split(' ');
+        if (parts.length !== 2) res.json('Access denied');
+        var [ scheme, token ] = parts;
+
+        if (!/^Bearer$/i.test(scheme)) res.json('Access denied');
+        const device = await Device.findOne({ token });
+        if(!device) {
+            res.json('Access denied')
+        } else if (device.UA ==! UA) {
+            res.json('Unidentified device')
+        } else {
+            jwt.verify(token, secret, async (err, decoded) => {
+                if (err) {
+                    res.json('Access denied')
+                } else {
+                    device.lastActive = new Date();
+                    await device.save();
+                    req.accountId = decoded.id;
+                    req.device = device.id;
+                    return next()
+                }
+            })
+        }
+    }
+}
