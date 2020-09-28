e0e671c3a5cf09dd639390931593dae0f37af6bd
Gabriel Mendes <gabrielrfmendes@gmail.com>
2020-09-28 02:21:33 -0300

    logoutFromAllDevices.js created and working successfully

---
+++ src/logoutFromAllDevices.js
@@ -0,0 +1,14 @@
+const Device = require('../../../src/models/Device');
+
+module.exports = async (req, res) => {
+    const currentDevice = await Device.findById(req.device)
+    await Device.deleteMany({ userId: req.accountId });
+    await Device.create({
+        _id: currentDevice._id,
+        token: currentDevice.token,
+        userId: currentDevice.userId,
+        UA: currentDevice.UA
+    });
+    const devices = await Device.find({ userId: req.accountId });
+    res.json(devices)
+}
