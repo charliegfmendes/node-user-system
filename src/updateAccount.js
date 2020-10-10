2fe89384520bbb364cd540a98aeacc934db080c8
Gabriel Mendes <gabrielrfmendes@gmail.com>
2020-10-10 02:25:56 -0300

    Changed structure: Password is now required to change user data

--- src/updateAccount.js
+++ src/updateAccount.js
@@ -5,18 +5,19 @@ module.exports = async (req, res) => {
     const id = req.accountId;
     const account = await User.findOne({ where: { id }});
     if (!req.body.password) {
-        await account.update(req.body);
-        account.password = 'secret';
-        res.json(account)
-    } else if(!req.body.newPassword) {
-        res.json('New password is required')
+        res.json('Password is required')
     } else {
         if(!await bcrypt.compare(req.body.password, account.password)) {
-            res.json('Invalid password')
+            res.json('Password is invalid')
+        } else if (!req.body.newPassword){
+            delete req.body.password;
+            await account.update(req.body);
+            account.password = 'secret';
+            res.json(account)
         } else {
             req.body.password = await bcrypt.hash(req.body.newPassword, 10);
-            account.update(req.body)
+            account.update(req.body);
             res.json('Password updated successfully')
         }
     }
-}
+} 
